15fd07a6f792299581acb59951805bf999bb1026
Tlandreth3 <31454456+Tlandreth3@users.noreply.github.com>
2017-08-30 22:11:31 -0600

    Declaring Variables Part 2

--- Declaring_Variable_1/Program.cs
+++ Declaring_Variable_1/Program.cs
@@ -6,24 +6,53 @@ namespace Declaring_Variables_1
 	{
 		public static void Main(string[] args)
 		{
-			/* Variables are something to store data in, so that you can put the data
+            /* Variables are something to store data in, so that you can store the data
              * into a bucket and retrieve it easily from the same bucket.
              */
+
+            /*
 			int x;
 			int y;
 			// we are establishing variables here, the "int" signifies that these variables
 			// will have numerical integer values
 
-			x = 7;
+			x = 3;
 			y = x + 7;
+
 
 			//the eual sign is an operator
 			// here we are assigning variables values
 
 			Console.WriteLine(y);
-			Console.ReadLine();
-
 			//Now we command the console to read y, which is 10
+			*/
+
+            /*
+			String myFirstName;
+                // We are declaring myFirstName as a string
+                //This is called camel case (lower case first word uppercase all others)
+                //variable names are case sensitive
+                //When using a "var" declaration type, you are essentially letting the computer 
+                //decipeher what type of varibale it is
+            myFirstName = "Trace";
+                //we are now declaring our variable
+            Console.WriteLine(myFirstName);
+            */
+
+
+            // Data conversion
+
+            int x = 7;
+            string y = "Trace";
+            string myFirstTry = x.ToString() + y;
+
+            //.NET IS smart enough to cancatinate the integer 7 into a string that says "7"
+
+            int myScondTry = x + y;
+            Console.WriteLine(myFirstTry);
+			
+
+			
 
 
 		}
